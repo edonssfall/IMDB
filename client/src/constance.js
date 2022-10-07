150b918b33b25d6eafc797bdce5429dc8d1aa411
Leonid Tsarov <edonssfall123@gmail.com>
2022-10-07 15:55:27 +0200

    v0.4.6

--- client/src/constance.js
+++ client/src/constance.js
@@ -1,2 +1,2 @@
-export let DjangoAPIHost = 'http://localhost:8000/';
-// export let DjangoAPIHost = 'http://54.144.106.13/';
+//export let DjangoAPIHost = 'http://localhost:8000/';
+export let DjangoAPIHost = 'http://54.210.80.56/';
