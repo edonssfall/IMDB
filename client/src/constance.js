5ac04c7a73201969dba2026f95c5cbaf53dc7707
Leonid Tsarov <edonssfall123@gmail.com>
2022-10-17 10:02:20 +0200

    v0.5.9_ssl

--- client/src/constance.js
+++ client/src/constance.js
@@ -1,5 +1,4 @@
 export let DjangoAPIHost = 'http://localhost:8000/';
-//AWS Host
-//export let DjangoAPIHost = 'http://34.229.166.218/';
 //GoogleCloud
 //export let DjangoAPIHost = 'http://34.116.156.45/';
+//export let DjangoAPIHost = 'https://ithillel-leonid.ogir-ok.com/';
